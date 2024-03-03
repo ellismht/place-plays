168aed9de7d29ee092d36448c4b19ff3ac88d603
Maciej Rosochacki <ellis.mht@gmail.com>
2024-03-03 16:11:02 +0100

    test endpoints

--- PlacePlays.WebApi/Endpoints/SpotifyEndpoints.cs
+++ PlacePlays.WebApi/Endpoints/SpotifyEndpoints.cs
@@ -6,10 +6,18 @@ public static class SpotifyEndpoints
     {
         group.MapGet("TestGetInfo", TestGetInfo);
         
+        group.MapGet("GetAuth", GetAuth);
+        
         return group;
     }
 
     public static async Task<IResult> TestGetInfo()
+    {
+
+        return TypedResults.Ok();
+    }
+    
+    public static async Task<IResult> GetAuth()
     {
 
         return TypedResults.Ok();
