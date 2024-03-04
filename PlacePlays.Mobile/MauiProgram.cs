a7b0b1e050a2486ea685c9b8704ed3872cfa733f
Maciej Rosochacki <>
2024-03-04 21:39:47 +0100

    spotifyAuth added

--- PlacePlays.Mobile/MauiProgram.cs
+++ PlacePlays.Mobile/MauiProgram.cs
@@ -1,5 +1,4 @@
-﻿using CommunityToolkit.Maui;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace PlacePlays.Mobile;
 
@@ -10,13 +9,11 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
 
 #if DEBUG
         builder.Logging.AddDebug();
