b75f1369d3d103e0c653a4c5daf669ebd4302eec
Maciej Rosochacki <ellis.mht@gmail.com>
2024-03-06 20:03:10 +0100

    added maui proj with simple auth

---
+++ PlacePlays.Mobile/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace PlacePlays.Mobile;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        
+       Browser.Default.OpenAsync(new Uri("https://accounts.spotify.com/authorize?response_type=token&client_id=-&scope=user-read-currently-playing&redirect_uri=pp://auth&state=123"),BrowserLaunchMode.SystemPreferred);
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
---
+++ PlacePlays.Mobile/Platforms/Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace PlacePlays.Mobile;
+
+[Activity(Theme = "@style/Maui.SplashTheme", LaunchMode = LaunchMode.SingleTop, MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[IntentFilter([Intent.ActionView],
+    DataScheme = "pp",
+    DataHost = "auth",
+    AutoVerify = true,
+    Categories = [ Intent.ActionView, Intent.CategoryDefault, Intent.CategoryBrowsable ])]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnNewIntent(Intent intent)
+    {
+        base.OnNewIntent(intent);
+
+        var smth = 0;
+    }
+}
