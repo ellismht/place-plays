0b7ffc7b9f872eec5add5c254964da9afc25190c
Maciej Rosochacki <ellis.mht@gmail.com>
2024-03-17 15:40:17 +0100

    litedb context

---
+++ PlacePlays.Infrastructure/DAL/LiteDB/BaseLiteDbContext.cs
@@ -0,0 +1,40 @@
+using LiteDB;
+
+namespace PlacePlays.Infrastructure.DAL.LiteDB;
+
+public abstract class BaseLiteDbContext<T> : IDisposable
+{
+    public ILiteCollection<T> Collection { get; private set; }
+    private LiteDatabase _db;
+    private bool _isDisposed = true;
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    
+    private void Dispose(bool disposing)
+    {
+        if (_isDisposed) return;
+        if (disposing)
+        {
+            _db?.Dispose();
+            _db = null;
+        }
+
+        _isDisposed = true;
+    }
+    
+    protected void Create(string liteDbLocation, string collectionName)
+    {
+        if (_db is not null) return;
+        
+        var localization = $@"\\{Environment.MachineName}" + liteDbLocation;
+        _db = new LiteDatabase($"Filename={localization}; Connection=Direct;");
+        Collection = _db.GetCollection<T>(collectionName);
+        _isDisposed = false;
+    }
+    
+    ~BaseLiteDbContext() => Dispose(false);
+}
--- PlacePlays.Infrastructure/DAL/LiteDB/SpotifyLiteDBContext.cs
+++ PlacePlays.Infrastructure/DAL/LiteDB/SpotifyLiteDBContext.cs
@@ -1,33 +1,15 @@
-using LiteDB;
 using Microsoft.Extensions.Options;
 using PlacePlays.Application.Models;
 using PlacePlays.Application.Models.Options;
 
 namespace PlacePlays.Infrastructure.DAL.LiteDB;
 
-public class SpotifyLiteDbContext : IDisposable
+public class SpotifyLiteDbContext : BaseLiteDbContext<SpotifyLiteDbModel>
 {
-    public ILiteCollection<SpotifyLiteDbModel> Collection { get; private set; } = null;
-    private readonly LiteDbOptions _options;
-    private LiteDatabase _db = null;
     private const string CollectionName = "Spotify";
+
     public SpotifyLiteDbContext(IOptions<LiteDbOptions> options)
     {
-        _options = options.Value;
-        Create();
-    }
-
-    private void Create()
-    {
-        if (_db is not null) return;
-        
-        var localization = $@"\\{Environment.MachineName}" + _options.SpotifyDbLocation;
-        _db = new LiteDatabase($"Filename={localization}; Connection=Direct;");
-        Collection = _db.GetCollection<SpotifyLiteDbModel>(CollectionName);
-    }
-
-    public void Dispose()
-    {
-        _db.Dispose();
+        Create(options.Value.SpotifyDbLocation, CollectionName);
     }
 }
