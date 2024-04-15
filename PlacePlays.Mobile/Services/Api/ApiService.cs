15e886b640e434576cd645c9abd590e2f61ba247
Maciej Rosochacki <ellis.mht@gmail.com>
2024-04-15 20:26:27 +0200

    some changes

--- PlacePlays.Mobile/Services/Api/ApiService.cs
+++ PlacePlays.Mobile/Services/Api/ApiService.cs
@@ -8,26 +8,11 @@ namespace PlacePlays.Mobile.Services.Api;
 
 public class ApiService : IApiService
 {
-    private const string BaseGroupEndpoint = "/api";
+    private const string BaseGroupEndpoint = "api";
     private const string PostSpotifyRecordEndpoint = "/saveTrackLocationInfo";
     
     public async ValueTask PostSpotifyRecord(SpotifyRecord record)
     {
-        //TODO: remove
-        try
-        {
-            var client = new HttpClient();
-            var json = JsonSerializer.Serialize(record);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var res = await client.PostAsync("http://10.0.2.2:5176/api/saveTrackLocationInfo", content);
-        }
-        catch(Exception e)
-        {
-            var smth = e;
-            throw;
-        }
-        //TODO
-
-        var response = await App.ApiClient.PostAsJsonAsync(BaseGroupEndpoint + PostSpotifyRecordEndpoint, record);
+        var response = await App.ApiClient.PostAsJsonAsync($"{BaseGroupEndpoint}{PostSpotifyRecordEndpoint}", record);
     }
 }
